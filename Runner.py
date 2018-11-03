27614b096b43c1401894907d2bb0d9ec15b3ac40
Viresh Gupta <viresh16118@iiitd.ac.in>
2018-11-03 09:29:17 +0530

    Loser agent
    The agent learnt to do nothing and just like a random agent,
    It ends up blowing itself up
    Pushing this just for historic purposes
    
    This agent's qtable was trained over 1500 episodes

--- Runner.py
+++ Runner.py
@@ -32,7 +32,7 @@ def main():
         state = env.reset()
         done = False
         while not done:
-            # env.render()
+            env.render()
             actions = env.act(state)
             state, reward, done, info = env.step(actions)
             # print(reward)
